519072132e5cea82abf2b120cea932791ecaaf69
kua0t <urielzalazar100@gmail.com>
2024-08-24 20:42:38 -0300

    .env.template added

--- src/config.js
+++ src/config.js
@@ -1,17 +1,9 @@
 export const PORT = process.env.PORT || 4000;
 
 export const MONGODB_URI =
-<<<<<<< HEAD
-  process.env.MONGODB_URI || "mongodb+srv://kuaotyt:SZK0BYG2AjYQOlUj@clientcndb.qa9tq.mongodb.net/?retryWrites=true&w=majority&appName=ClientCNDB";
-
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
-
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://cerrajeria-frontend.vercel.app";
-=======
   process.env.MONGODB_URI ||
   "mongodb+srv://kuaotyt:SZK0BYG2AjYQOlUj@clientcndb.qa9tq.mongodb.net/?retryWrites=true&w=majority&appName=ClientCNDB";
 
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
->>>>>>> f60704d (test en localhost)
