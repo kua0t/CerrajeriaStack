49ceac1640e53ee1b1966ef69be74f8b10da2aab
Uriel <129794989+kua0t@users.noreply.github.com>
2024-08-24 19:56:09 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
->>>>>>> 95e7f2a5f929b9694b40e765f519e431ea6571f9
