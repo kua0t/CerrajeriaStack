e114b073d079ac8db6ef02f0a63e8a05878e7261
kua0t <urielzalazar100@gmail.com>
2024-08-24 20:18:54 -0300

    test en localhost

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 export const API_URL = "https://cerrajeria-api.vercel.app";
+=======
+export const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+>>>>>>> f60704d (test en localhost)
