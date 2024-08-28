c19fe562e77da477ec4ed8c173f99774191234ac
Uriel <129794989+kua0t@users.noreply.github.com>
2024-08-28 11:24:12 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(
   cors({
     origin: FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
