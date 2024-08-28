fccd92bcf5ca68f57c68b19062f01afe126c7e9b
Uriel <129794989+kua0t@users.noreply.github.com>
2024-08-28 10:08:02 -0300

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,9 +11,7 @@ import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
 const router = Router();
 
 router.post("/register", validateSchema(registerSchema), register);
-router.post('/login', (req, res) => {
-  res.send('Recibido');
-});
+router.post("/login", validateSchema(loginSchema), login);
 router.get("/verify", verifyToken);
 router.post("/logout", verifyToken, logout);
 
