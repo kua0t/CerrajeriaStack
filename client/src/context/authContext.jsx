8cadd18c9e375f3d2a07e5b6ca927c60818bcfc8
Uriel <129794989+kua0t@users.noreply.github.com>
2024-08-28 11:40:19 -0300

    Update authContext.jsx

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -46,6 +46,7 @@ export const AuthProvider = ({ children }) => {
       setIsAuthenticated(true);
     } catch (error) {
       console.log(error);
+      // setErrors(error.response.data.message);
     }
   };
 
