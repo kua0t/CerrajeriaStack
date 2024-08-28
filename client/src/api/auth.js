1d41424aebde605847131b71ca08bc0364a280e2
Uriel <129794989+kua0t@users.noreply.github.com>
2024-08-28 10:42:47 -0300

    Update auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,15 +3,8 @@ import axios from "./axios";
 export const registerRequest = async (user) =>
   axios.post(`/auth/register`, user);
 
-export const loginRequest = async (user) => {
-  try {
-    const response = await axios.post(`/auth/login`, user);
-    return response.data;
-  } catch (error) {
-    console.error("Error during login:", error.response?.data || error.message);
-    throw error;
-  }
-};
+export const loginRequest = async (user) => 
+  axios.post(`/auth/login`, user);
 
 
 export const verifyTokenRequest = async () => axios.get(`/auth/verify`);
