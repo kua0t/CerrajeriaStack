68722844c04517769be2cc276979f7d458bc8144
Uriel <129794989+kua0t@users.noreply.github.com>
2024-09-03 19:43:07 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
-import { FRONTEND_URL } from "./config.js";
+import { FRONTEND_URL, PORT } from "./config.js";
+import { connectDB } from "./db.js";
 
 const app = express();
 
@@ -25,14 +25,28 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api", taskRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  const path = await import("path");
-  app.use(express.static("client/dist"));
+async function startServer() {
+  try {
+    await connectDB();
+    console.log(`Environment: ${process.env.NODE_ENV}`);
 
-  app.get("*", (req, res) => {
-    console.log(path.resolve("client", "dist", "index.html"));
-    res.sendFile(path.resolve("client", "dist", "index.html"));
-  });
+    if (process.env.NODE_ENV === "production") {
+      const path = (await import("path")).default;
+      app.use(express.static("client/dist"));
+
+      app.get("*", (req, res) => {
+        console.log(path.resolve("client", "dist", "index.html"));
+        res.sendFile(path.resolve("client", "dist", "index.html"));
+      });
+    }
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export default app;
+startServer();
