3f72614975c953f212b864d91dd72db45577bdbc
Uriel <129794989+kua0t@users.noreply.github.com>
2024-09-03 19:44:15 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import app from "./app.js";
-import { PORT } from "./config.js";
-import { connectDB } from "./db.js";
+// import app from "./app.js";
+// import { PORT } from "./config.js";
+// import { connectDB } from "./db.js";
 
-async function main() {
-  try {
-    await connectDB();
-    console.log(`Environment: ${process.env.NODE_ENV}`);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// async function main() {
+  // try {
+    // await connectDB();
+    // console.log(`Environment: ${process.env.NODE_ENV}`);
+  // } catch (error) {
+   //  console.error(error);
+//   }
+// }
 
-export default app;
+// export default app;
 
-main();
+//  main();
