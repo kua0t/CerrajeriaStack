2d4da66fda98d3b8ef7607669f41d7a8510d2cef
Uriel <129794989+kua0t@users.noreply.github.com>
2024-09-17 13:33:22 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,14 +9,11 @@ import { connectDB } from "./db.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: FRONTEND_URL,
-    methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: 'https://cerrajeria-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(morgan("dev"));
