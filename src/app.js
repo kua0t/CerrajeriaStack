237eeece3a949fab080ac07eef5d71098e5b8bdd
Uriel <129794989+kua0t@users.noreply.github.com>
2024-09-19 20:53:58 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -27,6 +27,10 @@ app.post('/api/recursos', (req, res) => {
    res.status(200).json({ message: 'Recurso creado correctamente' });
 });
 
+app.get('/api/recursos', (req, res) => {
+   res.status(200).json({ message: 'Recurso obtenido correctamente' });
+});
+
 app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
