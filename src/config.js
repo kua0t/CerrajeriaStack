beb5bae7434932d0e767f11a3db635980cfb025d
Uriel <129794989+kua0t@users.noreply.github.com>
2025-08-07 16:07:23 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ export const FRONTEND_URL = "https://cerrajeria-stack.vercel.app";
 
 export const PORT = 4000;
 
+export const MONGODB_URI = "mongodb+srv://kuaotyt:KtJi6nUIu1sKb1Ip@clientcndb.qa9tq.mongodb.net/?retryWrites=true&w=majority&appName=ClientCNDB";
 
 export const TOKEN_SECRET = "secret";
